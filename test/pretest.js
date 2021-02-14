135e63e3a3b1a367fa42aa36bfada014a207dfbf
Stephane Demingongo Litemo <sdemingongo@gmail.com>
2021-02-15 00:11:56 +0100

    mocha test

--- test/pretest.js
+++ test/pretest.js
@@ -1,4 +1,1 @@
-require('chai/register-assert'); // Using Assert style
-require('chai/register-expect'); // Using Expect style
-require('chai/register-should'); // Using Should style
 require('debug').enable('logger:test*'); // enable debug logs
